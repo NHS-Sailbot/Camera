241708939b05e7f5d55fb715a24e0442e830c064
GabeRundlett <gabe@rundlett.com>
2020-01-20 20:24:58 -0500

    Update

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -1,26 +1,30 @@
 #pragma once
 
 namespace Henry {
-    class Camera {
-      private:
-        enum Flags { NONE = 0, OPEN_STATUS = 1, VALID_STATUS = 2 };
-        unsigned int mWidth, mHeight, mDataSize;
-        unsigned short mID, mFlags;
-        void (*mOnUpdate)(const Camera &);
-        unsigned char *mData;
+	class Camera {
+	  private:
+		enum Flags {
+			NONE = 0,
+			OPEN_STATUS = 1,
+			VALID_STATUS = 2
+		};
+		unsigned int mWidth, mHeight, mDataSize;
+		unsigned short mId, mFlags;
+		void (*mOnUpdate)(const Camera &);
+		unsigned char *mData;
 
-      public:
-        unsigned char *mPixelData;
+	  public:
+		unsigned char *mPixelData;
 
-        Camera();
-        Camera(const unsigned int aWidth, const unsigned int aHeight);
-        ~Camera();
+		Camera();
+		Camera(const unsigned int width, const unsigned int height);
+		~Camera();
 
-        void Open(const unsigned int aWidth, const unsigned int aHeight);
-        void Update();
-        void Close();
-        inline void OnUpdate(void (*const aFunc)(const Camera &)) { mOnUpdate = aFunc; }
-        inline bool IsOpen() const { return mFlags & OPEN_STATUS; };
-        inline bool IsValid() const { return mFlags & VALID_STATUS; };
-    };
+		void open(const unsigned int width, const unsigned int height);
+		void update();
+		void close();
+		inline void onUpdate(void (*const f)(const Camera &)) { mOnUpdate = f; }
+		inline bool isOpen() const { return mFlags & OPEN_STATUS; };
+		inline bool isValid() const { return mFlags & VALID_STATUS; };
+	};
 } // namespace Henry
--- src/LinuxCamera.cpp
+++ src/LinuxCamera.cpp
@@ -1,9 +1,5 @@
 #include "Camera.hpp"
 
-#define DEBUG_ENABLE_TIMING
-#define DEBUG_ENABLE_LOGGING
-#include <Debug/Debug.hpp>
-
 #include <asm/types.h>
 #include <linux/videodev2.h>
 
@@ -18,238 +14,194 @@
 #include <errno.h>
 #include <string.h>
 
+#include <chrono>
+
 namespace Henry {
-    static constexpr unsigned int BUFFER_COUNT = 4;
-    struct LinuxCameraDevice {
-        v4l2_buffer mBufferInfo[BUFFER_COUNT];
-        void *mBufferStart[BUFFER_COUNT];
-        int mBufferIndex = 0, mDeviceFileID = -1;
-        unsigned long long mCurrentTime, mLastTickTime;
-    };
-    static LinuxCameraDevice sLinuxDevices[32];
-    struct jpeg_decompress_struct sJpegInfo;
-    struct jpeg_error_mgr sJpegError;
-    static void defaultCameraUpdate(const Camera &c) {}
+	static constexpr unsigned int BUFFER_COUNT = 4, FRAMES_PER_SECOND = 30;
+	static constexpr unsigned long long FRAME_INTERVAL = 1000000000 / TICKS_PER_SECOND;
 
-    Camera::Camera()
-        : mWidth(0), mHeight(0), mDataSize(0), mID(0), mFlags(NONE), mOnUpdate(defaultCameraUpdate), mData(nullptr),
-          mPixelData(nullptr) {}
+	struct LinuxCameraDevice {
+		v4l2_buffer mBufferInfo[BUFFER_COUNT];
+		void *mBufferStart[BUFFER_COUNT];
+		int mBufferIndex = 0, mDeviceFileID = -1;
+		unsigned long long mCurrentTime, mLastTickTime;
+	};
+	static LinuxCameraDevice sLinuxDevices[32];
+	struct jpeg_decompress_struct sJpegInfo;
+	struct jpeg_error_mgr sJpegError;
+	static void defaultCameraUpdate(const Camera &c) {}
 
-    Camera::Camera(const unsigned int width, const unsigned int height)
-        : mWidth(width), mHeight(height), mDataSize(0), mID(0), mFlags(NONE), mOnUpdate(defaultCameraUpdate), mData(nullptr),
-          mPixelData(nullptr) {
-        Open(width, height);
-    }
+	using namespace std::chrono;
+	static auto sGlobalStart = steady_clock::now();
+	static inline unsigned long long now() { return duration_cast<nanoseconds>(steady_clock::now() - sGlobalStart).count(); }
+	static inline void sleep(const unsigned long long ns) {
+		const auto tStartTime = now();
+		while (now() - tStartTime < ns) {}
+	}
 
-    Camera::~Camera() {
-        Close();
-        delete[] mPixelData;
-    }
+	Camera::Camera() :
+	mWidth(0), mHeight(0), mDataSize(0), mId(0), mFlags(NONE), mOnUpdate(defaultCameraUpdate), mData(nullptr),
+	mPixelData(nullptr) {}
 
-    void Camera::Open(const unsigned int width, const unsigned int height) {
-        DEBUG_BEGIN_FUNC_PROFILE;
+	Camera::Camera(const unsigned int width, const unsigned int height) :
+	mWidth(width), mHeight(height), mDataSize(0), mId(0), mFlags(NONE), mOnUpdate(defaultCameraUpdate), mData(nullptr),
+	mPixelData(nullptr) {
+		open(width, height);
+	}
 
-        mWidth = width, mHeight = height;
-        if (mPixelData) delete[] mPixelData;
-        mPixelData = new unsigned char[mWidth * mHeight * 3];
+	Camera::~Camera() {
+		close();
+		delete[] mPixelData;
+	}
 
-        DEBUG_BEGIN_PROFILE(dSelectIndex);
-        unsigned int tIndex = 0;
-        for (; tIndex < 32; ++tIndex)
-            if (sLinuxDevices[tIndex].mDeviceFileID < 0) break;
-        if (tIndex > 31) {
-            Debug::Log::error("Too many devices requested!");
-            mFlags &= ~OPEN_STATUS;
-            return;
-        }
-        DEBUG_END_PROFILE(dSelectIndex);
+	void Camera::open(const unsigned int width, const unsigned int height) {
+		mWidth = width, mHeight = height;
+		if (mPixelData) delete[] mPixelData;
+		mPixelData = new unsigned char[mWidth * mHeight * 3];
+		unsigned int tIndex = 0;
+		for (; tIndex < 32; ++tIndex)
+			if (sLinuxDevices[tIndex].mDeviceFileID < 0) break;
+		if (tIndex > 31) {
+			mFlags &= ~OPEN_STATUS;
+			return;
+		}
 
-        auto &tLinuxDevice = sLinuxDevices[tIndex];
-        const char *const tFilepath = "/dev/video0";
-        Debug::Log::message("Attempting to open camera at %s...", tFilepath);
+		auto &tLinuxDevice = sLinuxDevices[tIndex];
+		const char *const tFilepath = "/dev/video0";
 
-        DEBUG_BEGIN_PROFILE(dOpenDeviceFile);
-        tLinuxDevice.mDeviceFileID = ::open(tFilepath, O_RDWR);
-        if (!tLinuxDevice.mDeviceFileID) {
-            Debug::Log::error("unable to open video source");
-            mFlags &= ~OPEN_STATUS;
-            return;
-        }
-        DEBUG_END_PROFILE(dOpenDeviceFile);
+		tLinuxDevice.mDeviceFileID = ::open(tFilepath, O_RDWR);
+		if (!tLinuxDevice.mDeviceFileID) {
+			mFlags &= ~OPEN_STATUS;
+			return;
+		}
 
-        DEBUG_BEGIN_PROFILE(dVideo4LinuxGetCapabilities);
-        v4l2_capability tCapability;
-        if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_QUERYCAP, &tCapability)) {
-            Debug::Log::error("unable to query video capability");
-            close(tLinuxDevice.mDeviceFileID);
-            mFlags &= ~OPEN_STATUS;
-            return;
-        }
-        if (!(tCapability.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
-            Debug::Log::error("device does not handle single-planar video capture");
-            close(tLinuxDevice.mDeviceFileID);
-            mFlags &= ~OPEN_STATUS;
-            return;
-        }
-        DEBUG_END_PROFILE(dVideo4LinuxGetCapabilities);
+		v4l2_capability tCapability;
+		if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_QUERYCAP, &tCapability)) {
+			::close(tLinuxDevice.mDeviceFileID);
+			mFlags &= ~OPEN_STATUS;
+			return;
+		}
+		if (!(tCapability.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
+			::close(tLinuxDevice.mDeviceFileID);
+			mFlags &= ~OPEN_STATUS;
+			return;
+		}
 
-        DEBUG_BEGIN_PROFILE(dVideo4LinuxSetFormat);
-        v4l2_format tFormat;
-        tFormat.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        tFormat.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
-        tFormat.fmt.pix.width = mWidth;
-        tFormat.fmt.pix.height = mHeight;
-        if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_S_FMT, &tFormat) < 0) {
-            Debug::Log::error("unable to set video capture format");
-            close(tLinuxDevice.mDeviceFileID);
-            mFlags &= ~OPEN_STATUS;
-            return;
-        }
-        DEBUG_END_PROFILE(dVideo4LinuxSetFormat);
+		v4l2_format tFormat;
+		tFormat.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		tFormat.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+		tFormat.fmt.pix.width = mWidth;
+		tFormat.fmt.pix.height = mHeight;
+		if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_S_FMT, &tFormat) < 0) {
+			::close(tLinuxDevice.mDeviceFileID);
+			mFlags &= ~OPEN_STATUS;
+			return;
+		}
 
-        DEBUG_BEGIN_PROFILE(dVideo4LinuxSetFramerate);
-        v4l2_streamparm tStreamParam;
-        tStreamParam.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        tStreamParam.parm.capture.timeperframe.numerator = 1;
-        tStreamParam.parm.capture.timeperframe.denominator = 30;
-        if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_S_PARM, &tStreamParam) == -1 ||
-            ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_G_PARM, &tStreamParam) == -1) {
-            Debug::Log::error("unable to set framerate");
-            mFlags &= ~OPEN_STATUS;
-            return;
-        }
-        DEBUG_END_PROFILE(dVideo4LinuxSetFramerate);
+		v4l2_streamparm tStreamParam;
+		tStreamParam.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		tStreamParam.parm.capture.timeperframe.numerator = 1;
+		tStreamParam.parm.capture.timeperframe.denominator = FRAMES_PER_SECOND;
+		if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_S_PARM, &tStreamParam) == -1 ||
+		ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_G_PARM, &tStreamParam) == -1) {
+			mFlags &= ~OPEN_STATUS;
+			return;
+		}
 
-        DEBUG_BEGIN_PROFILE(dVideo4LinuxPrepareBuffers);
-        v4l2_requestbuffers tBufRequest;
-        tBufRequest.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        tBufRequest.memory = V4L2_MEMORY_MMAP;
-        tBufRequest.count = 4;
-        if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_REQBUFS, &tBufRequest) < 0) {
-            Debug::Log::error("unable to request video buffers");
-            close(tLinuxDevice.mDeviceFileID);
-            mFlags &= ~OPEN_STATUS;
-            return;
-        }
-        for (unsigned char i = 0; i < BUFFER_COUNT; ++i) {
-            tLinuxDevice.mBufferInfo[i].type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-            tLinuxDevice.mBufferInfo[i].memory = V4L2_MEMORY_MMAP;
-            tLinuxDevice.mBufferInfo[i].index = i;
-            if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_QUERYBUF, &tLinuxDevice.mBufferInfo[i]) < 0) {
-                Debug::Log::error("unable to query video buffers");
-                close(tLinuxDevice.mDeviceFileID);
-                mFlags &= ~OPEN_STATUS;
-                return;
-            }
-            tLinuxDevice.mBufferStart[i] = mmap(NULL, tLinuxDevice.mBufferInfo[i].length, PROT_READ | PROT_WRITE, MAP_SHARED,
-                                                tLinuxDevice.mDeviceFileID, tLinuxDevice.mBufferInfo[i].m.offset);
-            if (tLinuxDevice.mBufferStart[i] == MAP_FAILED) {
-                Debug::Log::error("mmap: %s", strerror(errno));
-                mFlags &= ~OPEN_STATUS;
-                return;
-            }
-            memset(tLinuxDevice.mBufferStart[i], 0, tLinuxDevice.mBufferInfo[i].length);
-        }
-        DEBUG_END_PROFILE(dVideo4LinuxPrepareBuffers);
+		v4l2_requestbuffers tBufRequest;
+		tBufRequest.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		tBufRequest.memory = V4L2_MEMORY_MMAP;
+		tBufRequest.count = BUFFER_COUNT;
+		if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_REQBUFS, &tBufRequest) < 0) {
+			::close(tLinuxDevice.mDeviceFileID);
+			mFlags &= ~OPEN_STATUS;
+			return;
+		}
+		for (unsigned char i = 0; i < BUFFER_COUNT; ++i) {
+			tLinuxDevice.mBufferInfo[i].type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+			tLinuxDevice.mBufferInfo[i].memory = V4L2_MEMORY_MMAP;
+			tLinuxDevice.mBufferInfo[i].index = i;
+			if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_QUERYBUF, &tLinuxDevice.mBufferInfo[i]) < 0) {
+				::close(tLinuxDevice.mDeviceFileID);
+				mFlags &= ~OPEN_STATUS;
+				return;
+			}
+			tLinuxDevice.mBufferStart[i] = mmap(NULL, tLinuxDevice.mBufferInfo[i].length, PROT_READ | PROT_WRITE, MAP_SHARED,
+			tLinuxDevice.mDeviceFileID, tLinuxDevice.mBufferInfo[i].m.offset);
+			if (tLinuxDevice.mBufferStart[i] == MAP_FAILED) {
+				mFlags &= ~OPEN_STATUS;
+				return;
+			}
+			memset(tLinuxDevice.mBufferStart[i], 0, tLinuxDevice.mBufferInfo[i].length);
+		}
 
-        DEBUG_BEGIN_PROFILE(dVideo4LinuxActivateStreaming);
-        int tType = tLinuxDevice.mBufferInfo[0].type;
-        if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_STREAMON, &tType) < 0) {
-            Debug::Log::error("VIDIOC_STREAMON: %s", strerror(errno));
-            mFlags &= ~OPEN_STATUS;
-            return;
-        }
-        DEBUG_END_PROFILE(dVideo4LinuxActivateStreaming);
+		int tType = tLinuxDevice.mBufferInfo[0].type;
+		if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_STREAMON, &tType) < 0) {
+			mFlags &= ~OPEN_STATUS;
+			return;
+		}
 
-        DEBUG_BEGIN_PROFILE(dVideo4LinuxPutBufferInQueue);
-        for (unsigned char i = 1; i < BUFFER_COUNT; ++i) {
-            if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_QBUF, &tLinuxDevice.mBufferInfo[i]) < 0) {
-                Debug::Log::error("VIDIOC_QBUF: %s", strerror(errno));
-                mFlags &= ~OPEN_STATUS;
-                return;
-            }
-        }
-        DEBUG_END_PROFILE(dVideo4LinuxPutBufferInQueue);
+		for (unsigned char i = 1; i < BUFFER_COUNT; ++i) {
+			if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_QBUF, &tLinuxDevice.mBufferInfo[i]) < 0) {
+				mFlags &= ~OPEN_STATUS;
+				return;
+			}
+		}
 
-        DEBUG_BEGIN_PROFILE(dLibJpegInit);
-        sJpegInfo.err = jpeg_std_error(&sJpegError);
-        jpeg_create_decompress(&sJpegInfo);
-        DEBUG_END_PROFILE(dLibJpegInit);
+		sJpegInfo.err = jpeg_std_error(&sJpegError);
+		jpeg_create_decompress(&sJpegInfo);
 
-        mID = tIndex;
-        mFlags |= OPEN_STATUS;
-        Debug::Log::success("Opened camera '%s' at %dx%d", tFilepath, mWidth, mHeight);
-        tLinuxDevice.mCurrentTime = Debug::Timer::Micros::now();
-        tLinuxDevice.mLastTickTime = tLinuxDevice.mCurrentTime;
-    }
+		mId = tIndex;
+		mFlags |= OPEN_STATUS;
+		tLinuxDevice.mCurrentTime = now();
+		tLinuxDevice.mLastTickTime = tLinuxDevice.mCurrentTime;
+	}
 
-    void Camera::Update() {
-        auto &tLinuxDevice = sLinuxDevices[mID];
-        tLinuxDevice.mCurrentTime = Debug::Timer::Micros::now();
-        const auto tElapsed = tLinuxDevice.mCurrentTime - tLinuxDevice.mLastTickTime;
+	void Camera::update() {
+		auto &tLinuxDevice = sLinuxDevices[mId];
+		tLinuxDevice.mCurrentTime = now();
+		const auto tElapsed = tLinuxDevice.mCurrentTime - tLinuxDevice.mLastTickTime;
 
-        if (tElapsed > 33333) {
-            DEBUG_BEGIN_FUNC_PROFILE;
+		if (tElapsed > FRAME_INTERVAL) {
+			if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_QBUF, &sLinuxDevices[mId].mBufferInfo[tLinuxDevice.mBufferIndex]) < 0) return;
 
-            if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_QBUF, &sLinuxDevices[mID].mBufferInfo[tLinuxDevice.mBufferIndex]) <
-                0) {
-                Debug::Log::error("VIDIOC_QBUF2: %s", strerror(errno));
-                return;
-            }
+			++tLinuxDevice.mBufferIndex;
+			if (tLinuxDevice.mBufferIndex == BUFFER_COUNT) tLinuxDevice.mBufferIndex = 0;
 
-            ++tLinuxDevice.mBufferIndex;
-            if (tLinuxDevice.mBufferIndex == BUFFER_COUNT) tLinuxDevice.mBufferIndex = 0;
+			if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_DQBUF, &tLinuxDevice.mBufferInfo[tLinuxDevice.mBufferIndex]) < 0) return;
 
-            if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_DQBUF, &tLinuxDevice.mBufferInfo[tLinuxDevice.mBufferIndex]) < 0) {
-                Debug::Log::error("VIDIOC_DQBUF: %s", strerror(errno));
-                return;
-            }
+			mData = reinterpret_cast<unsigned char *>(tLinuxDevice.mBufferStart[tLinuxDevice.mBufferIndex]);
+			mDataSize = tLinuxDevice.mBufferInfo[tLinuxDevice.mBufferIndex].length;
 
-            mData = reinterpret_cast<unsigned char *>(tLinuxDevice.mBufferStart[tLinuxDevice.mBufferIndex]);
-            mDataSize = tLinuxDevice.mBufferInfo[tLinuxDevice.mBufferIndex].length;
+			if (!(mData == nullptr) && mDataSize > 0) {
+				jpeg_mem_src(&sJpegInfo, mData, mDataSize);
+				int rc = jpeg_read_header(&sJpegInfo, TRUE);
+				jpeg_start_decompress(&sJpegInfo);
+				while (sJpegInfo.output_scanline < sJpegInfo.output_height) {
+					unsigned char *temp_array[] = {mPixelData + (sJpegInfo.output_scanline) * mWidth * 3};
+					jpeg_read_scanlines(&sJpegInfo, temp_array, 1);
+				}
+				jpeg_finish_decompress(&sJpegInfo);
+			}
 
-            if (!(mData == nullptr) && mDataSize > 0) {
-                DEBUG_BEGIN_PROFILE(libjpeg_start);
+			mOnUpdate(*this);
 
-                jpeg_mem_src(&sJpegInfo, mData, mDataSize);
-                int rc = jpeg_read_header(&sJpegInfo, TRUE);
-                jpeg_start_decompress(&sJpegInfo);
+			if (tElapsed > FRAME_INTERVAL + 100000)
+				tLinuxDevice.mLastTickTime = now();
+			else
+				tLinuxDevice.mLastTickTime += FRAME_INTERVAL;
+		}
+	}
 
-                while (sJpegInfo.output_scanline < sJpegInfo.output_height) {
-                    unsigned char *temp_array[] = {mPixelData + (sJpegInfo.output_scanline) * mWidth * 3};
-                    jpeg_read_scanlines(&sJpegInfo, temp_array, 1);
-                }
-
-                DEBUG_END_PROFILE(libjpeg_start);
-                DEBUG_BEGIN_PROFILE(libjpeg_end);
-
-                jpeg_finish_decompress(&sJpegInfo);
-
-                DEBUG_END_PROFILE(libjpeg_end);
-            }
-
-            mOnUpdate(*this);
-
-            if (tElapsed > 34000)
-                tLinuxDevice.mLastTickTime = Debug::Timer::Micros::now();
-            else
-                tLinuxDevice.mLastTickTime += 33333;
-        }
-    }
-
-    void Camera::Close() {
-        DEBUG_BEGIN_FUNC_PROFILE;
-
-        if (IsOpen()) {
-            jpeg_destroy_decompress(&sJpegInfo);
-            auto &tLinuxDevice = sLinuxDevices[mID];
-            int type = tLinuxDevice.mBufferInfo[0].type;
-            if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_STREAMOFF, &type) < 0) {
-                Debug::Log::error("VIDIOC_STREAMOFF: %s", strerror(errno));
-                return;
-            }
-            close(tLinuxDevice.mDeviceFileID);
-            tLinuxDevice.mDeviceFileID = -1;
-        }
-    }
+	void Camera::close() {
+		if (isOpen()) {
+			jpeg_destroy_decompress(&sJpegInfo);
+			auto &tLinuxDevice = sLinuxDevices[mId];
+			int type = tLinuxDevice.mBufferInfo[0].type;
+			if (ioctl(tLinuxDevice.mDeviceFileID, VIDIOC_STREAMOFF, &type) < 0)
+				return;
+			::close(tLinuxDevice.mDeviceFileID);
+			tLinuxDevice.mDeviceFileID = -1;
+		}
+	}
 } // namespace Henry
